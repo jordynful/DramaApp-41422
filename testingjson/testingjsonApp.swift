d94e43ca1e9e02cf985b1fd75e7eba6c4e3d9a2e
Jordyn Fulbright <jordynfulbright@gmail.com>
2022-04-13 13:21:35 -0400

    Initial Commit

---
+++ testingjson/testingjsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingjsonApp.swift
+//  testingjson
+//
+//  Created by Jordyn Fulbright on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct testingjsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
